16cd16e6e734452bdeedadc39984e36b9409bbf5
judrummer <judrummercpe@gmail.com>
2017-01-14 15:35:29 +0700

    fix INIT action bug

--- lib/devtools-redux-kotlin/src/main/kotlin/com/github/kittinunf/redux/devTools/redux_kotlin/DevTools.kt
+++ lib/devtools-redux-kotlin/src/main/kotlin/com/github/kittinunf/redux/devTools/redux_kotlin/DevTools.kt
@@ -3,6 +3,7 @@ package com.github.kittinunf.redux.devTools.redux_kotlin
 import com.github.kittinunf.redux.devTools.core.Instrument
 import com.github.kittinunf.redux.devTools.core.InstrumentOption
 import com.github.kittinunf.redux.devTools.core.defaultOption
+import redux.INIT
 import redux.api.Reducer
 import redux.api.Store
 
@@ -15,11 +16,15 @@ fun <S> devTools(option: InstrumentOption = defaultOption()): Store.Enhancer<S> 
             val instrument = Instrument<S>(option, store.state)
             instrument.start()
             store.replaceReducer { state, action ->
-                val reducedState = reducer.reduce(state, action)
-                if (instrument.isMonitored && (action !is DevToolsStateChangeAction)) {
-                    instrument.handleStateChangeFromAction(reducedState, action)
+                if (action != INIT) {
+                    val reducedState = reducer.reduce(state, action)
+                    if (instrument.isMonitored && (action !is DevToolsStateChangeAction)) {
+                        instrument.handleStateChangeFromAction(reducedState, action)
+                    }
+                    reducedState
+                } else {
+                    state
                 }
-                reducedState
             }
             object : Store<S> {
 
